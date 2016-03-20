a44da3c71013ebd29a0c2ff782785c4bb0837514
ttemel <Hoca@192.168.1.98>
2016-03-20 13:59:38 +0200

    sdfg

--- EYS/src/main/java/org/tutev/envanterys/entity/Kisi.java
+++ EYS/src/main/java/org/tutev/envanterys/entity/Kisi.java
@@ -15,6 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -44,6 +45,8 @@ public class Kisi extends BaseEntity {
 	@SuppressWarnings("unused")
 	private String adSoyad;
 	private Dosya resim;
+
+	
 	
 	
 	public Kisi() {
